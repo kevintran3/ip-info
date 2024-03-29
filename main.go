39648b957e4d83329c644d2bfdf0e658ee569398
Kevin Tran <kevin@kt3.us>
2024-03-28 23:40:25 -0400

    fix: use seeip api

--- main.go
+++ main.go
@@ -5,25 +5,27 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	"slices"
 	"strings"
 )
 
 type IP struct {
-	Ip          string `json:"query"`
-	City        string `json:"city"`
-	Isp         string `json:"isp"`
-	Org         string `json:"org"`
-	As          string `json:"as"`
-	RegionName  string `json:"regionName"`
-	Region      string `json:"region"`
-	Country     string `json:"country"`
-	CountryCode string `json:"countryCode"`
-	Zip         string `json:"zip"`
-	Timezone    string `json:"timezone"`
-	Lat         string `json:"lat"`
-	Lon         string `json:"lon"`
-	IsPrivate   bool   `json:"isPrivate,omitempty"`
-	Str         string `json:"str,omitempty"`
+	Ip            string `json:"ip"`
+	City          string `json:"city"`
+	Organization  string `json:"organization"`
+	Asn           string `json:"asn"`
+	Region        string `json:"region"`
+	RegionCode    string `json:"region_code"`
+	Country       string `json:"country"`
+	CountryCode   string `json:"country_code"`
+	CountryCode3  string `json:"country_code3"`
+	ContinentCode string `json:"continent_code"`
+	PostalCode    string `json:"postal_code"`
+	Timezone      string `json:"timezone"`
+	Latitude      string `json:"latitude"`
+	Longitude     string `json:"longitude"`
+	IsPrivate     bool   `json:"isPrivate,omitempty"`
+	Str           string `json:"str,omitempty"`
 }
 
 func ParseIP(ipStr string) IP {
@@ -37,15 +39,29 @@ func ParseIP(ipStr string) IP {
 			}
 		}
 	}
-	resp, _ := http.Get("http://ip-api.com/json/" + ipStr)
+
+	apiUrl := fmt.Sprintf("https://api.seeip.org/geoip/%s", ipStr)
+	if slices.Contains([]string{"ipv4", "ipv6"}, ipStr) {
+		apiUrl = fmt.Sprintf("https://%s.seeip.org/geoip/", ipStr)
+	}
+
+	resp, _ := http.Get(apiUrl)
 	data := IP{Ip: ipStr}
 	_ = json.NewDecoder(resp.Body).Decode(&data)
-	data.Str = fmt.Sprintf("%s - %s - %s", data.Ip, data.Isp, strings.Join([]string{data.City, data.RegionName, data.Country}, ", "))
+	data.Str = fmt.Sprintf("%s - %s - %s", data.Ip, data.Organization, strings.Join([]string{data.City, data.Region, data.Country}, ", "))
 	return data
 }
 
 func ParseMyIP() IP {
 	return ParseIP("")
+}
+
+func ParseMyIPv4() IP {
+	return ParseIP("ipv4")
+}
+
+func ParseMyIPv6() IP {
+	return ParseIP("ipv6")
 }
 
 func ClientIP(req *http.Request) string {
